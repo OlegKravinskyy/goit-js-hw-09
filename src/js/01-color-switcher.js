8447312f308779afa313e8b365c9074623ce732b
OlegKravinskyy <kravinskyy@gmail.com>
2022-09-13 00:13:20 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const COLOR_DELAY = 1000;
+let timerId = null;
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+refs.btnStart.addEventListener('click', onButtonStart);
+
+refs.btnStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, COLOR_DELAY);
+
+  refs.btnStart.setAttribute('disabled', 'disabled');
+}
+
+function onButtonStop() {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+}
